e5d86093751fc8f57d9b2b2fbffef7bb5db90f95
Abdisalam <abdisalamfresh@gmail.com>
2023-04-10 22:54:58 -0700

    adding the theme when the user enters the page

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const icon = document.getElementById("dark-light-icon");
 
 
 darkModeBtn.addEventListener('click', () => {
+    icon.classList.remove();
     const body = document.querySelector('body');
-    const isDarkMode = body.classList.toggle('dark-mode')
+    const isDarkMode = body.classList.toggle('dark-mode');
+    
 
     if (isDarkMode){
         icon.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+
     }else {
         icon.innerHTML = `<i class="fa-solid fa-lightbulb"></i>`;
     }
+
+    icon.toggleAttribute('hidden', isDarkMode)
 
     const links = document.querySelectorAll('a');
     links.forEach(link => {
