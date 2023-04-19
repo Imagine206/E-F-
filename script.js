e8d1c4c6d0a45abe5a4f2b6f3c95e9d36622ec8b
Abdisalam <abdisalamfresh@gmail.com>
2023-04-18 23:03:09 -0700

    made minor changes to the button

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const darkModeBtn = document.getElementById("dark-mode")
-const iconLight = document.getElementById("dark-light-icon-light")
-const iconDark = document.getElementById("dark-light-icon-dark")
+// const darkModeBtn = document.getElementById("dark-mode")
+// const iconLight = document.getElementById("dark-light-icon-light")
+// const iconDark = document.getElementById("dark-light-icon-dark")
 
-darkModeBtn.addEventListener('click', (e) => {
+// darkModeBtn.addEventListener('click', (e) => {
 
-  const body = document.querySelector('body');
-  const isDarkMode = body.classList.toggle('dark-mode');
+//   const body = document.querySelector('body');
+//   const isDarkMode = body.classList.toggle('dark-mode');
 
-  if (isDarkMode) {
-    iconLight.style.display = "none" // Hide the light icon
-    iconDark.style.display = "inline-block" // Show the dark icon
-  } else {
-    iconLight.style.display = "inline-block" // Show the light icon
-    iconDark.style.display = "none" // Hide the dark icon
-  }
+//   if (isDarkMode) {
+//     iconLight.style.display = "none" // Hide the light icon
+//     iconDark.style.display = "inline-block" // Show the dark icon
+//   } else {
+//     iconLight.style.display = "inline-block" // Show the light icon
+//     iconDark.style.display = "none" // Hide the dark icon
+//   }
 
-  const links = document.querySelectorAll("a")
-  links.forEach((link) => {
-    link.style.color = isDarkMode ? "white" : "black"
-  })
-})
+//   const links = document.querySelectorAll("a")
+//   links.forEach((link) => {
+//     link.style.color = isDarkMode ? "white" : "black"
+//   })
+// })
